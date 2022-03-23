256296d4b212303a36e1970ce2ed6523042e480f
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-03-23 17:00:17 +0000

    Added program

--- main.cs
+++ main.cs
@@ -1,7 +1,21 @@
+// Cristiano
+// ICS2O-Unit2-04-CSharp
+// March 23 2022
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int length;
+        int width;
+        Console.WriteLine("Enter the length of your rectangle:");
+        length = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the width of your rectangle:");
+        width = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Your rectangle's perimeter is " + ((length * 2) + (width * 2)) + "cm");
+        Console.WriteLine("Your rectangle's area is " + (length * width) + "cm²");
+        Console.WriteLine("\nFinished.");
+    }
 }
